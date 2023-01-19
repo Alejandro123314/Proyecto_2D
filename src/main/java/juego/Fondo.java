df956d9364e1df425fc1ffca6a8b6207c1991d2d
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2023-01-19 11:38:59 +0000

    cambios al fondo del mapa

--- src/main/java/juego/Fondo.java
+++ src/main/java/juego/Fondo.java
@@ -1,5 +1,24 @@
 package juego;
 
-public class Fondo {
+import javafx.scene.canvas.GraphicsContext;
+
+public class Fondo extends ObjetoJuego {
+
+	public Fondo(int x, int y, String nombreImagen, int velocidad) {
+		super(x, y, nombreImagen, velocidad);
+		
+	}
+
+	@Override
+	public void pintar(GraphicsContext graficos) {
+		graficos.drawImage(Juego.imagenes.get(nombreImagen), x, y);
+		
+	}
+
+	@Override
+	public void mover() {
+		/*movimiento de fondo queda para implementar con personaje en la clase Jugador*/
+		
+	}
 
 }
--- src/main/java/juego/Juego.java
+++ src/main/java/juego/Juego.java
@@ -1,66 +1,115 @@
 package juego;
 
-
+import java.util.HashMap;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Juego extends Application {
-	
+
 	private GraphicsContext graficos;
 	private Group root;
 	private Scene scene;
-	private Canvas lienzo; 
-	
+	private Canvas lienzo;
+	private Fondo fondo;
+	private static boolean arriba;
+	private static boolean abajo;
+	private static boolean derecha;
+	private static boolean izquierda;
+	public static HashMap<String, Image> imagenes;
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-			inicializarComponentes();
-			gestionEventos();
-			cicloJuego();
-			pintar();
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Monkey Brosh");
-			primaryStage.show();	
+		inicializarComponentes();
+		gestionEventos();
+		cicloJuego();
+		pintar();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Monkey Brosh");
+		primaryStage.show();
 	}
-	
+
 	public void cargarImagenes() {
-		
+		imagenes.put("BosqueVerde", new Image("images/BosqueVerde.png"));
 
 	}
-	
+
 	public void cicloJuego() {
-		
+
 	}
-	
+
 	public void actualizarEstado() {
-		
+		fondo.mover();
 	}
-	
 
 	public void inicializarComponentes() {
+		imagenes = new HashMap<String, Image>();
+		cargarImagenes();
+		fondo = new Fondo(0, 0, "BosqueVerde", 0);
 		root = new Group();
 		scene = new Scene(root, 800, 600);
-		lienzo = new Canvas(1000,1000);
+		lienzo = new Canvas(1000, 1000);
 		root.getChildren().add(lienzo);
 		graficos = lienzo.getGraphicsContext2D();
 	}
-	
+
 	public void pintar() {
-		graficos.drawImage(new Image("images/BosqueVerde.png"),0,0);
-		
-	}
-	
-	private void gestionEventos() {
-		
+		fondo.pintar(graficos);
+		graficos.drawImage(imagenes.get("BosqueVerde"), 0, 0);
+
 	}
 
+	private void gestionEventos() {
+		/* implementar para pruebas con el mapa */
+
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent evento) {
+				switch (evento.getCode().toString()) {
+				case "RIGHT":
+					derecha = true;
+					break;
+				case "LEFT":
+					izquierda = true;
+					break;
+				case "UP":
+					arriba = true;
+					break;
+				case "DOWN":
+					abajo = true;
+				}
+			}
+		});
+
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent evento) {
+				switch (evento.getCode().toString()) {
+				case "RIGHT":
+					derecha = false;
+					break;
+				case "LEFT":
+					izquierda = false;
+					break;
+				case "UP":
+					arriba = false;
+					break;
+				case "DOWN":
+					abajo = false;
+				}
+			}
+		});
+
+	}
 }
